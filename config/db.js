d3c56f43632f0109381bbf910cf845cebe3b28e7
¨dylmarcor¨ <dylancorbett@icloud.com>
2022-03-14 10:41:29 -0700

    change db connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,20 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
 
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
-db.once('open', () => {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// db.once('open', () => {
+//   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dylmarcor:<password>@cluster0.vzuth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
