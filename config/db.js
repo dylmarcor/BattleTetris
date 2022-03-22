c60d70b6c17a154d1f4a100c5c03d933ff91a910
¨dylmarcor¨ <dylancorbett@icloud.com>
2022-03-22 11:40:32 -0700

    move back to mongoose for db

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
-// var mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
-// var db = mongoose.connection;
+var db = mongoose.connection;
 
-// db.once('open', () => {
-//   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
-// });
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+db.once('open', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
